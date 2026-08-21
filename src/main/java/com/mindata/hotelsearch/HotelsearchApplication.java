115bcbad94b2f7b393ca78355291c127a9320731
MirkoGabriel <mirkogabriel67@gmail.com>
2026-08-21 14:36:59 -0300

    first commit

---
+++ src/main/java/com/mindata/hotelsearch/HotelsearchApplication.java
@@ -0,0 +1,14 @@
+package com.mindata.hotelsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelsearchApplication.class, args);
+        System.out.println("Mirko");
+    }
+
+}
