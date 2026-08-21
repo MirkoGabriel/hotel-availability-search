ca9cd573c943905b613ffe696b602ec5b4d34b97
MirkoGabriel <mirkogabriel67@gmail.com>
2026-08-21 15:08:55 -0300

    add jacoco configuration to gradle to coverge

--- src/main/java/com/mindata/hotelsearch/HotelsearchApplication.java
+++ src/main/java/com/mindata/hotelsearch/HotelsearchApplication.java
@@ -2,13 +2,14 @@ package com.mindata.hotelsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class HotelsearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HotelsearchApplication.class, args);
-        System.out.println("Mirko");
     }
 
 }
